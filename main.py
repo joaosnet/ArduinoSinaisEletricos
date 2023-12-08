afe9641f0ce73234cd307518efc9ff868b5f0db9
João Natividade <87316339+joaosnet@users.noreply.github.com>
2023-12-08 01:34:49 +0000

    nada funcionou

--- main.py
+++ main.py
@@ -1,40 +1,102 @@
-try:
-    from pyfirmata import Arduino, util
-except ModuleNotFoundError:
-    import pip
-    # pip install pyfirmata
-    pip.main(['install', 'pyfirmata'])
+#!/usr/bin/python3
 
-import time
+# Copyright (c) 2012, Fabian Affolter <fabian@affolter-engineering.ch>
+# Copyright (c) 2019-2021, Bernd Porr <mail@berndporr.me.uk>
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of the pyfirmata team nor the names of its contributors
+#   may be used to endorse or promote products derived from this software
+#   without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ''AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+# ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#
+# This program toggles the digital port 13 on/off every second.
+# Port 13 has an LED connected so you'll see def a flashing light!
+# Coding is done with a timer callback to avoid evil loops / delays.
 
-# Configuração da placa Arduino
-board = Arduino('COM2')  # Substitua 'COMX' pela porta do seu Arduino
+import pyfirmata2
+from threading import Timer
 
-# Configuração dos pinos para corrente e tensão
-pino_corrente = board.get_pin('a:0:i')  # Pino analógico A0 para corrente
-pino_tensao = board.get_pin('a:1:i')    # Pino analógico A1 para tensão
+class Blink():
+    def __init__(self, board, seconds):
+        # pin 13 which is connected to the internal LED
+        self.digital_0 = board.get_pin('d:13:o')
 
-# Configuração da taxa de amostragem
-taxa_amostragem = 1000  # 1 kHz
+        # flag that we want the timer to restart itself in the callback
+        self.timer = None
 
-# Tempo de espera para a estabilização do Arduino
-time.sleep(2)
+        # delay
+        self.DELAY = seconds
 
-# Função para ler e imprimir os valores de corrente e tensão
-def ler_sinais():
-    corrente = pino_corrente.read()
-    tensao = pino_tensao.read()
+    # callback function which toggles the digital port and
+    # restarts the timer
+    def blinkCallback(self):
+        # call itself again so that it runs periodically
+        self.timer = Timer(self.DELAY,self.blinkCallback)
 
-    if corrente is not None and tensao is not None:
-        print(f"Corrente: {corrente * 5} A")  # Supondo um sensor de corrente com escala 0-5V
-        print(f"Tensão: {tensao * 5} V")  # Supondo um sensor de tensão com escala 0-5V
+        # start the timer
+        self.timer.start()
+        
+        # now let's toggle the LED
+        v = self.digital_0.read()
+        v = not v
+        if v:
+            print("On")
+        else:
+            print("Off")
+        self.digital_0.write(v)
 
-# Loop principal
-try:
-    while True:
-        ler_sinais()
-        time.sleep(1 / taxa_amostragem)
+    # starts the blinking
+    def start(self):
+        # Kickstarting the perpetual timer by calling the
+        # callback function once
+        self.blinkCallback()
 
-except KeyboardInterrupt:
-    # Encerra o loop quando o usuário pressiona Ctrl+C
-    board.exit()
+    # stops the blinking
+    def stop(self):
+        # Cancel the timer
+        self.timer.cancel()
+
+# main program
+
+# Adjust that the port match your system, see samples below:
+# On Linux: /dev/ttyACM0,
+# On Windows: COM1, COM2, ...
+PORT =  'COM2'
+
+
+# Creates a new board
+board = pyfirmata2.Arduino(PORT)
+
+board.samplingOn(9600)
+
+t = Blink(board,1)
+t.start()
+
+print("To stop the program press return.")
+# Just blocking here to do nothing.
+input()
+
+t.stop()
+
+# close the serial connection
+board.exit()
