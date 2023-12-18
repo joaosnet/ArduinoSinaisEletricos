2f5091394958831326f4b716c178e1fe30f989b3
joaosnet <87316339+joaosnet@users.noreply.github.com>
2023-12-18 09:58:46 -0300

    Add third sensor reading to the serial output

--- ArduinoSinaisEletricos.ino
+++ ArduinoSinaisEletricos.ino
@@ -5,9 +5,10 @@ void setup() {
 void loop() {
   int valorSensor1 = analogRead(A0); // Lê o valor do primeiro sensor conectado ao pino A0
   int valorSensor2 = analogRead(A1); // Lê o valor do segundo sensor conectado ao pino A1
+  int valorSensor3 = analogRead(A2);
 
   // Formata os valores em uma string
-  String mensagem = String(valorSensor1) + " | " + String(valorSensor2);
+  String mensagem = String(valorSensor1) + " | " + String(valorSensor2) + " | " + String(valorSensor3);
 
   // Envia a string pela porta serial
   Serial.println(mensagem);
